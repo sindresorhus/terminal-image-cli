7b9627c4888f0d54a2adc81efb947e3998205b7d
Richie Bendall <richiebendall@gmail.com>
2020-07-19 00:22:14 +1200

    Support animated GIFs
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- cli.js
+++ cli.js
@@ -3,6 +3,7 @@
 const meow = require('meow');
 const getStdin = require('get-stdin');
 const terminalImage = require('terminal-image');
+const fileType = require('file-type');
 
 const cli = meow(`
 	Usage
@@ -23,8 +24,18 @@ if (!input && process.stdin.isTTY) {
 
 (async () => {
 	if (input) {
-		console.log(await terminalImage.file(input));
+		if ((await fileType.fromFile(input)).ext === 'gif') {
+			terminalImage.gifFile(input);
+		} else {
+			console.log(await terminalImage.file(input));
+		}
 	} else {
-		console.log(await terminalImage.buffer(await getStdin.buffer()));
+		const stdin = await getStdin.buffer();
+
+		if ((await fileType.fromBuffer(stdin)).ext === 'gif') {
+			terminalImage.gifBuffer(stdin);
+		} else {
+			console.log(await terminalImage.buffer(stdin));
+		}
 	}
 })();
